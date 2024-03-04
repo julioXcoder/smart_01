f257145af7c5ee937d6e9a8f4d2cde83f40fea95
Julio Njeza <julioxcoder@gmail.com>
2024-03-04 05:12:35 +0300

    Delete applicant-portal

--- server/actions/test/index.ts
+++
@@ -1,8 +0,0 @@
-"use server";
-
-export const doSomething = async () => {
-  // Simulate a delay of 5 seconds
-  await new Promise((resolve) => setTimeout(resolve, 5000));
-
-  // throw new Error("something went wrong");
-};
